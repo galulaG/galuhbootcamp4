cefc35d152f5734db86b96a5ae62d2c44e1c2859
galulaG <galuhhrUyahoo.com>
2018-01-24 19:59:47 +0700

    lanjut

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express"); 
+const bodyParser  = require("body-parser"); 
+const productRoutes = require("./routes/product"); 
+
+const fileUpload = require("express-fileupload"); 
+
+const app = express();
+
+app.use (function(req, res, next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(fileUpload());
+app.use(express.static('public'));
+
+
+
+app.use("/api/product", productRoutes());
+
+
+
+
+app.listen(process.env.PORT || 3000);
+//process.env.PORT supaya bisa pake sesuai port di server 
--- backend/models/product.js
+++ backend/models/product.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/productlist", { useMongoClient : true });
+mongoose.connect("mongodb://127.0.0.1:27017/productlist");
 
 const Schema = mongoose.Schema;
 
